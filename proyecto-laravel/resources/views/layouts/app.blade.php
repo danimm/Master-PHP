fb5ea3bdf390f84275f1e740d0ec0a71756f2213
Daniel Muñoz Martín <27631697+danimm@users.noreply.github.com>
2019-05-29 16:23:37 +0200

    Vista de imagenes del home con maquetación

--- proyecto-laravel/resources/views/layouts/app.blade.php
+++ proyecto-laravel/resources/views/layouts/app.blade.php
@@ -51,10 +51,10 @@
                             @endif
                         @else
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ url('/') }}">Inicio</a>
+                                <a class="nav-link" href="{{ route('home') }}">Inicio</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="/home">Subir Imagen</a>
+                                <a class="nav-link" href="{{ route('image.create')}}">Subir Imagen</a>
                             </li>
                             <li class="nav-item">
                                     @include('includes.avatar')
--- proyecto-laravel/routes/web.php
+++ proyecto-laravel/routes/web.php
@@ -42,3 +42,6 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/configuración', 'UserController@config')->name('config');
 Route::post('/user/update', 'UserController@update')->name('user.update');
 Route::get('/user/avatar/{filename}', 'UserController@getImage')->name('user.avatar');
+Route::get('/subir-imagen', 'ImageController@create')->name('image.create');
+Route::post('/image/save', 'ImageController@save')->name('image.save');
+Route::get('/image/file/{filename}', 'ImageController@getImage')->name('image.file');
